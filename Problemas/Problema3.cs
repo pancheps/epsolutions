8585bbfcc69312280432e2f4c83a95095e5ec29d
Panchito Rey <pancheps@hotmail.com>
2022-03-27 16:24:39 -0400

    First 5 problems

---
+++ Problemas/Problema3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems.Problemas
+{
+    internal class Problema3
+    {
+        public static long GetLargestPrimeLong(long num)
+        {
+            long biggestPrime = 0;
+            for (long i = 1; i <= Math.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                {
+                    if (IsPrime(i))
+                    {
+                        biggestPrime = i;
+                    }
+                }
+            }
+            return biggestPrime;
+        }
+
+        private static bool IsPrime(long num)
+        {
+            long counter = 0;
+            for (long i = 1; i < Math.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                {
+                    counter++;
+                }
+                if (counter > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int GetLargestPrimeInt(int num)
+        {
+            return Enumerable.Range(1, num).Select(individual => Enumerable.Range(1, (int)Math.Sqrt(individual)).Where(_ => _ % 2 != 0 || _ == 2).Aggregate(0, (accumulator, next) => individual % next == 0 ? accumulator += next : accumulator)).ToList().LastIndexOf(1) + 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Console.WriteLine(EulerProblems.Problemas.Problema1.GetMultiplesSum(1000));
+//Console.WriteLine(EulerProblems.Problemas.Problema2.GetFibonacciEvenSum(10));
+//Console.WriteLine(EulerProblems.Problemas.GetDupSocks.GetDups(15, new List<int> { 6 ,5 ,2 ,3 ,5 ,2 ,2 ,1 ,1 ,5 ,1 ,3 ,3 ,3 ,5 }));
+//Console.WriteLine(EulerProblems.Problemas.CountingValleys.ValleysCount(2, "DU"));
+//Console.WriteLine(EulerProblems.Problemas.Problema3.GetLargestPrimeLong(600851475143));
+//Console.WriteLine(EulerProblems.Problemas.Problema4.GetLargestPalindrome3x3());
+Console.WriteLine(EulerProblems.Problemas.Problema5.GetSmallestEvenlyDivisible(20));
+//Console.ReadLine();
