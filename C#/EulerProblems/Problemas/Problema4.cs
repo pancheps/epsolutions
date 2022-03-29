111655e49467beed0b25b7b8e31b6f4763762d7e
Panchito Rey <pancheps@hotmail.com>
2022-03-29 00:12:55 -0400

    Readd all files

---
+++ C#/EulerProblems/Problemas/Problema4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems.Problemas
+{
+    internal class Problema4
+    {
+        public static int GetLargestPalindrome3x3()
+        {
+            var palindrome = 1;
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = 100; j < 1000; j++)
+                {
+                    int res = i * j;
+                    if (IsPalindrome(res) && res > palindrome)
+                    {
+                        palindrome = res;
+                        Console.WriteLine($"Palindrome: {palindrome}");
+                    }
+                    Console.WriteLine(res);
+                }
+            }
+            return palindrome;
+        }
+
+        private static bool IsPalindrome(int num)
+        {
+            string str = num.ToString();
+            var charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return str == new string(charArray);
+        }
+    }
+}
---
+++ C#/EulerProblems/Problemas/Problema8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems.Problemas
+{
+    internal class Problema8
+    {
+        public static long Largest13Product(string number, int taken)
+        {
+            long result = 0;
+            number = number.Replace("\r\n", string.Empty);
+            for (int i = 0; i < number.Length - taken; i++)
+            {
+                var currentMult = number.Substring(i, taken).ToList().Aggregate(1L, (current, next) => long.Parse(current.ToString()) * long.Parse(next.ToString()));
+                result = currentMult > result ? currentMult : result;
+            }
+            return result;
+        }
+    }
+}
