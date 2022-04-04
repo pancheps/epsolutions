e4825c5ae9916ed74b6ef92b60459c7a8346b580
Jose Paz <jmpeace@gmail.com>
2022-04-04 17:49:20 -0400

    Euler 46

---
+++ JAVA/EulerProblems/Problemas/Util.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+
+public class Util {
+    public static void print(Collection<?> c) {
+            System.out.println(c);
+    }
+
+    public static void print(String c) {
+            System.out.println(c);
+    }
+}
